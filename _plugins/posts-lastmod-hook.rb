34ee24ae5bb521d4aaca4f370828fb146d5e3a4e
Lam Ngoc Khuong <me@ngockhuong.com>
2026-02-23 13:07:17 +0700

    fix: pin GitHub Action version and prevent shell injection in plugin
    - Pin cloudflare-purge-action from @master to @v0.3.0
    - Replace backtick shell commands with IO.popen in posts-lastmod-hook

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -4,10 +4,10 @@
 
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  commit_num = IO.popen(['git', 'rev-list', '--count', 'HEAD', post.path]) { |io| io.read }
 
   if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+    lastmod_date = IO.popen(['git', 'log', '-1', '--pretty=%ad', '--date=iso', post.path]) { |io| io.read }
     post.data['last_modified_at'] = lastmod_date
   end
 
